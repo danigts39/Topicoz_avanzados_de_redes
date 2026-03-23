731613ffa50bb4202480873c72a7c4a4666227a1
danigts39 <daniel.gts.soriano@gmail.com>
2026-03-23 16:33:49 -0600

    feat: :art:
    Practica1

--- import numpy as np.py
+++ import numpy as np.py
@@ -17,8 +17,8 @@ Listas para almacenar los datos
 
 """
 Latencias = []
-paquetas_enviados = []
-paquetas_recibidos = []
+paquetes_enviados = []
+paquetes_recibidos = []
 perdidos = 0
 
 print("Simulando tráfico de datos ....\n ")
@@ -40,22 +40,40 @@ for i in range (Num_paquetes):
         continue
 
     tiempo_recepcion = time.time()
-    latencia.append(tiempo_recepcion - tiempo_envio)
-    paquetas_enviados.append(Tamano_paquete)
-    paquetas_recibidos.append(Tamano_paquete)
+    Latencias.append(tiempo_recepcion - tiempo_envio)
+    paquetes_enviados.append(Tamano_paquete)
+    paquetes_recibidos.append(Tamano_paquete)
 
     """
     Metricas
     """
 
-    total_enviados = len(paquetas_enviados)
-    total_recibidos = len(paquetas_recibidos)
+    total_enviados = len(paquetes_enviados)
+    total_recibidos = len(paquetes_recibidos)
     tasa_perdida = perdidos / Num_paquetes 
     
-    latencia_promedio = np.mean(latencias)
+    latencia_promedio = np.mean(latencia)
 
-    throughput = (sum(paquetas_recibidos) / sum (latencia)) if latencias else 0
+    throughput = (sum(paquetes_recibidos) / sum (Latencias)) if Latencias else 0
 
+    """
+    Configuración de resultados
+    """
 
-        
+    print(f"Paquetes enviados: {total_enviados}")
+    print(f"Paquetes recibidos: {total_recibidos}")
+    print(f"Paquetes perdida: {perdidos}")
+    print(f"Tasa de perdidad: {tasa_perdida:.2f}")
+    print(f"Latencia promedio: {latencia_promedio:.4f}")
+    print(f"Throughput: {throughput:.2f} bytes/s")
 
+    """Grafica de latencias"""
+
+    plt.plot(Latencias)
+    plt.title("Latencia por paquete")
+    plt.xlabel("Paquetes")
+    plt.ylabel("Latencia(s)")
+    plt.grid()
+    plt.show()
+
+    
